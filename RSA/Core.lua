bd9a6e352e55bcec5e3565d26c2a540c6460cfaf
Caedilla <42666479+Caedilla@users.noreply.github.com>
2018-08-29 15:07:44 +0200

    InWarMode and InWorld now correctly function
    InWarMode was checking if the player is PvP Flagged, not War Mode Flagged.
    
    InWorld was also not checking War Mode status - it now checks it.

--- RSA/Core.lua
+++ RSA/Core.lua
@@ -172,7 +172,7 @@ function RSA.AnnouncementCheck() -- Checks against user settings to see if we ar
 	local LFParty = IsInGroup(LE_PARTY_CATEGORY_INSTANCE) -- party group found through group finder
 	local LFRaid = IsInRaid(LE_PARTY_CATEGORY_INSTANCE) -- raid grounp found through group finder
 	if RSA.db.profile.General.GlobalAnnouncements.OnlyInCombat and not InCombatLockdown() then return false end -- If we're not in combat and only announce in combat, stop right here.
-	if RSA.db.profile.General.GlobalAnnouncements.InWarMode and UnitIsPVP("player") == 1 and InInstance == false and InstanceType == "none" and not LFParty and not LFRaid then return true end -- Enable in World PvP.
+	if RSA.db.profile.General.GlobalAnnouncements.InWarMode and C_PvP.IsWarModeActive() and InstanceType == "none" and not LFParty and not LFRaid then return true end -- Enable in World PvP.
 	if RSA.db.profile.General.GlobalAnnouncements.Arena and InstanceType == "arena" then return true end
 	if RSA.db.profile.General.GlobalAnnouncements.Battlegrounds and LFRaid and (InstanceType == "pvp" or InstanceType == "none") then return true end
 	if RSA.db.profile.General.GlobalAnnouncements.InDungeon and InstanceType == "party" and not LFParty then return true end
@@ -180,7 +180,7 @@ function RSA.AnnouncementCheck() -- Checks against user settings to see if we ar
 	if RSA.db.profile.General.GlobalAnnouncements.InScenario and InstanceType == "scenario" == true then return true end
 	if RSA.db.profile.General.GlobalAnnouncements.InLFG_Party and (InstanceType == "party" and LFParty) then return true end
 	if RSA.db.profile.General.GlobalAnnouncements.InLFG_Raid and (InstanceType == "raid" and LFRaid) then return true end
-	if RSA.db.profile.General.GlobalAnnouncements.InWorld and InstanceType == "none" and not LFRaid then return true end -- Enable in World PvE.
+	if RSA.db.profile.General.GlobalAnnouncements.InWorld and InstanceType == "none" and not LFParty and not LFRaid and not C_PvP.IsWarModeActive() then return true end -- Enable in World PvE.
 	return false
 end
 
