90b0ad1ae88ce9fc1b0e6026376f17db306aeb3d
Caedilla <42666479+Caedilla@users.noreply.github.com>
2018-11-05 21:21:00 +0100

    Update whispers to new method.

--- RSA_Monk/Monk.lua
+++ RSA_Monk/Monk.lua
@@ -37,7 +37,8 @@ function RSA.Resurrect(_, _, target, _, caster)
 		end
 		if RSA.db.profile.Monk.Spells.Resuscitate.Whisper == true and dest ~= pName then
 			RSA.Replacements = {["[SPELL]"] = spellinfo, ["[LINK]"] = spelllinkinfo, ["[TARGET]"] = L["You"],}
-			RSA.Print_Whisper(string.gsub(message, ".%a+.", RSA.String_Replace), full_destName)
+			--RSA.Print_Whisper(string.gsub(message, ".%a+.", RSA.String_Replace), full_destName)
+			RSA.Print_Whisper(message, full_destName, RSA.Replacements, dest)
 			RSA.Replacements = {["[SPELL]"] = spellinfo, ["[LINK]"] = spelllinkinfo, ["[TARGET]"] = dest,}
 		end
 		if RSA.db.profile.Monk.Spells.Resuscitate.CustomChannel.Enabled == true then
