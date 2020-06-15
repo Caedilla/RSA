ca054abe92057891cb11d3b89568801e42eac9c0
Raeli <42666479+Caedilla@users.noreply.github.com>
2020-06-15 15:22:44 +0200

    update announcement preference order

--- RSA/Comms.lua
+++ RSA/Comms.lua
@@ -1,23 +1,16 @@
 local RSA = RSA or LibStub('AceAddon-3.0'):GetAddon('RSA')
 local L = LibStub('AceLocale-3.0'):GetLocale('RSA')
 
-local HighNumber = time()*time()
-local CanAnnounce
-local GroupMembers = {}
-local GroupLeader
-local GroupAssistants = {}
+local groupMembers = {}
+local groupAssistants = {}
 
 RSA.Comm = {}
-RSA.Comm.GroupStatus = {}
-
 
 local releasePriority = {
 	['alpha'] = 2,
 	['beta'] = 1,
 	['release'] = 0,
 }
-
-
 
 function RSA.Comm.Registry()
 	C_ChatInfo.RegisterAddonMessagePrefix('RSA')
@@ -72,8 +65,8 @@ function RSA.CheckGroupStatus(self, status)
 		RSA.SendCommMessage('RSA', 'RSA_Status', RSA.db.global.ID, (not IsInGroup(LE_PARTY_CATEGORY_HOME) and IsInGroup(LE_PARTY_CATEGORY_INSTANCE)) and 'INSTANCE_CHAT' or 'PARTY')
 	else
 		RSA.Comm.GroupAnnouncer = nil
-		wipe(GroupMembers)
-		wipe(GroupAssistants)
+		wipe(groupMembers)
+		wipe(groupAssistants)
 	end
 	if status == 'joinedGroup' then
 		RSA.VersionCheck('joinedGroup')
@@ -93,13 +86,48 @@ function RSA.OnStatusReceived(addon, prefix, message, channel, sender)
 		--['Bytes'] = tonumber(namebytes),
 		['ID'] = tonumber(message),
 	}
-	GroupMembers[sender] = SenderInfo
+	groupMembers[sender] = SenderInfo
 
-	for k, v in pairs(GroupMembers) do
-		if GroupMembers[k].ID < HighNumber then
-			RSA.Comm.GroupAnnouncer = GroupMembers[k].ID
+	for k, v in pairs(groupMembers) do
+		if UnitIsGroupLeader(groupMembers[k].Name) then
+			RSA.Comm.GroupAnnouncer = groupMembers[k].ID
+		end
+		if UnitIsGroupAssistant(groupMembers[k].Name) then
+			table.insert(groupAssistants, groupMembers[k].ID)
 		end
 	end
+
+	if not RSA.Comm.GroupAnnouncer then
+		if #groupAssistants > 0 then
+			local assistAnnouncer
+			for i = 1, #groupAssistants do
+				if i == 1 then
+					assistAnnouncer = groupAssistants[i]
+				elseif i > 1 then
+					if assistAnnouncer < groupAssistants[i] then
+						assistAnnouncer = groupAssistants[i]
+					end
+				end
+			end
+			RSA.Comm.GroupAnnouncer = assistAnnouncer
+		end
+	end
+
+	if not RSA.Comm.GroupAnnouncer then
+		local memberAnnouncer
+		for i = 1, #groupMembers do
+			if i == 1 then
+				memberAnnouncer = groupMembers[k].ID
+			elseif i > 1 then
+				if memberAnnouncer < groupMembers[k].ID then
+					memberAnnouncer = groupMembers[k].ID
+				end
+			end
+			RSA.Comm.GroupAnnouncer = memberAnnouncer
+		end
+	end
+
+
 	if RSA.Comm.GroupAnnouncer == tonumber(RSA.db.global.ID) then -- Group Announcer is me.
 	else -- Group Announcer is someone else.
 	end
@@ -108,8 +136,8 @@ end
 
 function RSA.GroupLeft()
 	RSA.Comm.GroupAnnouncer = nil
-	wipe(GroupMembers)
-	wipe(GroupAssistants)
+	wipe(groupMembers)
+	wipe(groupAssistants)
 end
 
 
