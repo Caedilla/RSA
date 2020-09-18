4887cc74eeff7d719498442d40160e15bc15c655
Raeli <42666479+Caedilla@users.noreply.github.com>
2020-09-18 19:58:31 +0200

    add remaining priest defaults

--- SpellData/Priest.lua
+++ SpellData/Priest.lua
@@ -44,12 +44,13 @@ local priestData = {
 	},
 	['dispelMagic'] = {
 		spellID = 528,
+		throttle = 0.25,
 		configDisplay = {
 			disabledChannels = {whisper = true},
 		},
 		events = {
 			['SPELL_DISPEL'] = {
-				messages = {"Dispelled [TARGET]'s [AURALINK]!",},
+				messages = {"Dispelled [TARGET]'s [EXTRALINK]!",},
 				tags = {
 					TARGET = true,
 					EXTRA = true,
@@ -108,6 +109,7 @@ local priestData = {
 	},
 	['guardianSpirit'] = {
 		spellID = 47788,
+		throttle = 0.25,
 		events = {
 			['SPELL_AURA_APPLIED'] = {
 				tracker = 2,
@@ -118,7 +120,10 @@ local priestData = {
 				uniqueSpellID = 48153,
 				tracker = 1,
 				messages = {"[LINK] prevented [TARGET]'s death and healed them for [AMOUNT]!",},
-				tags = {AMOUNT = true,},
+				tags = {
+					TARGET = true,
+					AMOUNT = true,
+				},
 			},
 			['SPELL_AURA_REMOVED'] = {
 				tracker = 1,
@@ -249,12 +254,263 @@ local priestData = {
 				tags = {TARGET = true,},
 			},
 			['RSA_SPELL_IMMUNE'] = { -- Fake event to easily generate options for immune specific messages.
-			messages = {"[TARGET] [MISSTYPE] [LINK]!"},
-			tags = {
-				TARGET = true,
-				MISSTYPE = true,
+				messages = {"[TARGET] [MISSTYPE] [LINK]!"},
+				tags = {
+					TARGET = true,
+					MISSTYPE = true,
+				},
 			},
 		},
+	},
+	['painSuppression'] = {
+		spellID = 33206,
+		events = {
+			['SPELL_AURA_APPLIED'] = {
+				messages = {"[LINK] cast on [TARGET]!",},
+				tags = {TARGET = true,},
+			},
+			['SPELL_AURA_REMOVED'] = {
+				messages = {"[LINK] on [TARGET] finished!",},
+				tags = {TARGET = true,},
+			},
+		},
+	},
+	['powerWordBarrier'] = {
+		spellID = 62618,
+		configDisplay = {
+			disabledChannels = {whisper = true},
+		},
+		events = {
+			['SPELL_CAST_SUCCESS'] = {
+				messages = {"[LINK] activated!",},
+			},
+			-- TODO: Support RSA_END_TIMER in monitor for spells without an appropriate event to hook into.
+			['RSA_END_TIMER'] = {
+				duration = 10,
+				messages = {"[LINK] finished!",},
+			},
+		},
+	},
+	['psychicHorror'] = {
+		spellID = 64044,
+		configDisplay = {
+			disabledChannels = {whisper = true},
+		},
+		events = {
+			['SPELL_AURA_APPLIED'] = {
+				messages = {"[LINK] cast on [TARGET]!",},
+				tags = {TARGET = true,},
+			},
+			['SPELL_AURA_REMOVED'] = {
+				messages = {"[LINK] on [TARGET] finished!",},
+				tags = {TARGET = true,},
+			},
+		},
+	},
+	['psychicScream'] = {
+		spellID = 8122,
+		configDisplay = {
+			disabledChannels = {whisper = true},
+		},
+		events = {
+			['SPELL_CAST_SUCCESS'] = {
+				messages = {"[LINK] activated!",},
+			},
+			['SPELL_AURA_REMOVED'] = {
+				messages = {"[LINK] finished!",},
+			},
+		},
+	},
+	['purify'] = {
+		spellID = 527,
+		throttle = 0.25,
+		additionalSpellIDs = {
+			[213634] = true, -- Purify Disease
+		},
+		events = {
+			['SPELL_DISPEL'] = {
+				messages = {"Cleansed [TARGET]'s [EXTRALINK]!",},
+				tags = {
+					TARGET = true,
+					EXTRA = true,
+				},
+			},
+		},
+	},
+	['rapture'] = {
+		spellID = 47536,
+		configDisplay = {
+			disabledChannels = {whisper = true},
+		},
+		events = {
+			['SPELL_CAST_SUCCESS'] = {
+				messages = {"[LINK] activated!",},
+			},
+			['SPELL_AURA_REMOVED'] = {
+				messages = {"[LINK] finished!",},
+			},
+		},
+	},
+	['rayOfHope'] = {
+		spellID = 197268,
+		throttle = 0.25,
+		events = {
+			['SPELL_AURA_APPLIED'] = {
+				tracker = 2,
+				messages = {"[LINK] cast on [TARGET]!",},
+				tags = {TARGET = true,},
+			},
+			['SPELL_HEAL'] = {
+				uniqueSpellID = 197336,
+				tracker = 1,
+				messages = {"[LINK] prevented [TARGET]'s death and healed them for [AMOUNT]!",},
+				tags = {
+					TARGET = true,
+					AMOUNT = true,
+				},
+			},
+			['SPELL_AURA_REMOVED'] = {
+				tracker = 1,
+				messages = {"[LINK] on [TARGET] finished!",},
+				tags = {TARGET = true,},
+			},
+		},
+	},
+	['resurrection'] = {
+		spellID = 2006,
+		events = {
+			['SPELL_RESURRECT'] = {
+				messages = {"Resurrected [TARGET]!",},
+				tags = {TARGET = true,},
+			},
+			['SPELL_CAST_START'] = {
+				messages = {"Casting [LINK] on [TARGET]!",},
+				tags = {TARGET = true,},
+			},
+		},
+	},
+	['shackleUndead'] = {
+		spellID = 9484,
+		configDisplay = {
+			disabledChannels = {whisper = true},
+		},
+		events = {
+			['SPELL_AURA_APPLIED'] = {
+				messages = {"[LINK] cast on [TARGET]!",},
+				tags = {TARGET = true,},
+			},
+			['SPELL_AURA_REMOVED'] = {
+				messages = {"[LINK] on [TARGET] finished!",},
+				tags = {TARGET = true,},
+			},
+
+			-- TODO: Check source is source of spell breaking, not source of the CC itself.
+			['SPELL_AURA_BROKEN_SPELL'] = {
+				messages = {"[SOURCE] removed [LINK] on [TARGET] with [EXTRALINK]!",},
+				tags = {
+					TARGET = true,
+					SOURCE = true,
+					EXTRA = true,
+				},
+			},
+			['SPELL_MISSED'] = {
+				messages = {"[LINK] [MISSTYPE] [TARGET]!",},
+				tags = {
+					TARGET = true,
+					MISSTYPE = true,
+				},
+			},
+			['RSA_SPELL_IMMUNE'] = {
+				messages = {"[TARGET] [MISSTYPE] [LINK]!"},
+				tags = {
+					TARGET = true,
+					MISSTYPE = true,
+				},
+			},
+		},
+	},
+	['shadowfiend'] = {
+		spellID = 34433,
+		additionalSpellIDs = {
+			[123040] = true, -- Mindbender Discipline
+			[200174] = true, -- Mindbender Shadow
+		},
+		configDisplay = {
+			disabledChannels = {whisper = true},
+		},
+		events = {
+			['SPELL_CAST_SUCCESS'] = {
+				messages = {"[LINK] activated!",},
+			},
+		},
+	},
+	['silence'] = {
+		spellID = 15487,
+		throttle = 0.25,
+		configDisplay = {
+			disabledChannels = {whisper = true},
+		},
+		events = {
+			['SPELL_INTERRUPT'] = {
+				uniqueSpellID = 220543,
+				messages = {"Interrupted [TARGET]'s [EXTRALINK]!",},
+				tags = {
+					TARGET = true,
+					EXTRA = true, -- Replaces AURA and TARSPELL.
+				},
+			},
+			['SPELL_AURA_APPLIED'] = {
+				messages = {"[LINK] cast on [TARGET]!",},
+				tags = {TARGET = true,},
+			},
+			['SPELL_AURA_REMOVED'] = {
+				messages = {"[LINK] on [TARGET] finished!",},
+				tags = {TARGET = true,},
+			},
+			['SPELL_MISSED'] = {
+				messages = {"[LINK] [MISSTYPE] [TARGET]!",},
+				tags = {
+					TARGET = true,
+					MISSTYPE = true,
+					EXTRA = true,
+				},
+			},
+			['RSA_SPELL_IMMUNE'] = { -- Fake event to easily generate options for immune specific messages.
+				messages = {"[TARGET] [MISSTYPE] [LINK]!"},
+				tags = {
+					TARGET = true,
+					MISSTYPE = true,
+				},
+			},
+		},
+	},
+	['symbolOfHope'] = {
+		spellID = 64901,
+		configDisplay = {
+			disabledChannels = {whisper = true},
+		},
+		events = {
+			['SPELL_CAST_SUCCESS'] = {
+				messages = {"[LINK] activated!",},
+			},
+			['SPELL_AURA_REMOVED'] = {
+				dest = {'player'},
+				messages = {"[LINK] finished!",},
+			},
+		},
+	},
+	['vampiricEmbrace'] = {
+		spellID = 15286,
+		configDisplay = {
+			disabledChannels = {whisper = true},
+		},
+		events = {
+			['SPELL_CAST_SUCCESS'] = {
+				messages = {"[LINK] activated!",},
+			},
+			['SPELL_AURA_REMOVED'] = {
+				messages = {"[LINK] finished!",},
+			},
 		},
 	},
 }
