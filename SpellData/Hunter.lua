737189f205d3f41eb6a009948dcdc0a6f9414d5c
Raeli <42666479+Caedilla@users.noreply.github.com>
2020-09-25 20:55:50 +0200

    add hunter spells

--- SpellData/Hunter.lua
+++ SpellData/Hunter.lua
@@ -1,1 +1,241 @@
 local RSA = LibStub('AceAddon-3.0'):GetAddon('RSA')
+
+local defaults = {
+	['aspectOfTheTurtle'] = {
+		spellID = 186265,
+		configDisplay = {
+			disabledChannels = {whisper = true},
+		},
+		events = {
+			['SPELL_CAST_SUCCESS'] = {
+				messages = {"[LINK] activated!",},
+			},
+			['SPELL_AURA_REMOVED'] = {
+				messages = {"[LINK] finished!",},
+			},
+		},
+	},
+	['bindingShot'] = {
+		spellID = 109248,
+		throttle = 0.5,
+		configDisplay = {
+			disabledChannels = {whisper = true},
+		},
+		events = {
+			['SPELL_AURA_APPLIED'] = {
+				uniqueSpellID = 117526,
+				messages = {"[LINK] activated!",},
+			},
+			['SPELL_AURA_REMOVED'] = {
+				uniqueSpellID = 117526,
+				messages = {"[LINK] finished!",},
+			},
+		},
+	},
+	['camouflage'] = {
+		spellID = 199483,
+		configDisplay = {
+			disabledChannels = {whisper = true},
+		},
+		events = {
+			['SPELL_CAST_SUCCESS'] = {
+				messages = {"[LINK] activated!",},
+			},
+			['SPELL_AURA_REMOVED'] = {
+				messages = {"[LINK] finished!",},
+			},
+		},
+	},
+	['concussiveShot'] = {
+		spellID = 5116,
+		configDisplay = {
+			disabledChannels = {whisper = true},
+		},
+		events = {
+			['SPELL_AURA_APPLIED'] = {
+				messages = {"[LINK] cast on [TARGET]!",},
+				tags = {TARGET = true,},
+			},
+			['SPELL_AURA_REMOVED'] = {
+				messages = {"[LINK] on [TARGET] finished!",},
+				tags = {TARGET = true,},
+			},
+		},
+	},
+	['counterShot'] = {
+		spellID = 147362,
+		configDisplay = {
+			disabledChannels = {whisper = true},
+		},
+		events = {
+			['SPELL_INTERRUPT'] = {
+				messages = {"Interrupted [TARGET]'s [EXTRALINK]!",},
+				tags = {
+					TARGET = true,
+					EXTRA = true,
+				},
+			},
+			['SPELL_MISSED'] = {
+				messages = {"[LINK] [MISSTYPE] [TARGET]!",},
+				tags = {
+					TARGET = true,
+					MISSTYPE = true,
+				},
+			},
+			['RSA_SPELL_IMMUNE'] = {
+				messages = {"[TARGET] [MISSTYPE] [LINK]!"},
+				tags = {
+					TARGET = true,
+					MISSTYPE = true,
+				},
+			},
+		},
+	},
+	--TODO: Check IDs
+	['freezingTrap'] = {
+		spellID = 187650,
+		configDisplay = {
+			disabledChannels = {whisper = true},
+		},
+		events = {
+			['SPELL_AURA_APPLIED'] = {
+				messages = {"[LINK] cast on [TARGET]!",},
+				tags = {TARGET = true,},
+			},
+			['SPELL_AURA_REMOVED'] = {
+				messages = {"[LINK] on [TARGET] finished!",},
+				tags = {TARGET = true,},
+			},
+			['SPELL_AURA_BROKEN_SPELL'] = {
+				messages = {"[SOURCE] removed [LINK] on [TARGET] with [EXTRALINK]!",},
+				tags = {
+					TARGET = true,
+					SOURCE = true,
+					EXTRA = true,
+				},
+			},
+			['SPELL_MISSED'] = {
+				messages = {"[LINK] [MISSTYPE] [TARGET]!",},
+				tags = {
+					TARGET = true,
+					MISSTYPE = true,
+				},
+			},
+			['RSA_SPELL_IMMUNE'] = {
+				messages = {"[TARGET] [MISSTYPE] [LINK]!"},
+				tags = {
+					TARGET = true,
+					MISSTYPE = true,
+				},
+			},
+		},
+	},
+	--TODO: Check IDs.
+	['intimidation'] = {
+		spellID = 19577,
+		configDisplay = {
+			disabledChannels = {whisper = true},
+		},
+		events = {
+			['SPELL_AURA_APPLIED'] = {
+				messages = {"[LINK] cast on [TARGET]!",},
+				tags = {TARGET = true,},
+			},
+			['SPELL_AURA_REMOVED'] = {
+				messages = {"[LINK] on [TARGET] finished!",},
+				tags = {TARGET = true,},
+			},
+		},
+	},
+	--TODO: Support custom code to tally amount of threat transferred.
+	['misdirection'] = {
+		spellID = 34477,
+		events = {
+			['SPELL_AURA_APPLIED'] = {
+				tracker = 2,
+				messages = {"[LINK] cast on [TARGET]!",},
+				tags = {TARGET = true,},
+			},
+			['SPELL_AURA_REMOVED'] = {
+				uniqueSpellID = 197336,
+				tracker = 1,
+				messages = {"[LINK] on [TARGET] finished and transferred [AMOUNT] threat!",},
+				tags = {
+					TARGET = true,
+					AMOUNT = true,
+				},
+			},
+		},
+	},
+	['muzzle'] = {
+		spellID = 187707,
+		configDisplay = {
+			disabledChannels = {whisper = true},
+		},
+		events = {
+			['SPELL_INTERRUPT'] = {
+				messages = {"Interrupted [TARGET]'s [EXTRALINK]!",},
+				tags = {
+					TARGET = true,
+					EXTRA = true,
+				},
+			},
+			['SPELL_MISSED'] = {
+				messages = {"[LINK] [MISSTYPE] [TARGET]!",},
+				tags = {
+					TARGET = true,
+					MISSTYPE = true,
+				},
+			},
+			['RSA_SPELL_IMMUNE'] = {
+				messages = {"[TARGET] [MISSTYPE] [LINK]!"},
+				tags = {
+					TARGET = true,
+					MISSTYPE = true,
+				},
+			},
+		},
+	},
+	['primalRage'] = {
+		spellID = 264667,
+		configDisplay = {
+			disabledChannels = {whisper = true},
+		},
+		events = {
+			['SPELL_CAST_SUCCESS'] = {
+				messages = {"[LINK] activated!",},
+			},
+			['SPELL_AURA_REMOVED'] = {
+				dest = {'player'},
+				messages = {"[LINK] finished!",},
+			},
+		},
+	},
+	['roarOfSacrifice'] = {
+		spellID = 53480,
+		events = {
+			['SPELL_AURA_APPLIED'] = {
+				messages = {"[LINK] cast on [TARGET]!",},
+				tags = {TARGET = true,},
+			},
+			['SPELL_AURA_REMOVED'] = {
+				messages = {"[LINK] on [TARGET] finished!",},
+				tags = {TARGET = true,},
+			},
+		},
+	},
+	['spiritMend'] = {
+		spellID = 90361,
+		events = {
+			['SPELL_HEAL'] = {
+				messages = {"[LINK] healed [TARGET] for [AMOUNT]!",},
+				tags = {
+					TARGET = true,
+					AMOUNT = true,
+				},
+			},
+		},
+	},
+}
+
+RSA.monitorData.hunter, RSA.configData.hunter = RSA.PrepareDataTables(defaults)
